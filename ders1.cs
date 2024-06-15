aa3198b6f40827126f6a2f937e432709f021a461
Httpsnazrin <147836567+Httpsnazrin@users.noreply.github.com>
2024-06-15 12:07:08 +0400

    Add files via upload

---
+++ ders1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        #region  Task1
+        //int number = Convert.ToInt32(Console.ReadLine());
+
+        //bool isFourDigit = number >= 1000 && number <= 9999;
+        //bool isDivisibleBy3Or7 = number % 3 == 0 || number % 7 == 0;
+
+        //if (isFourDigit && isDivisibleBy3Or7)
+        //{
+        //    Console.WriteLine("True");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("False");
+        //}
+        #endregion
+        #region Task2
+        //string name=Console.ReadLine();
+        //string myname = "Nazrin";
+        //if (name ==myname)
+        //{
+        //    Console.WriteLine("Yes");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("No");
+        //}
+        #endregion
+        #region Task3
+        //int n=Convert.ToInt32(Console.ReadLine());
+        //int lastDigit = n % 10;
+        //Console.WriteLine(lastDigit);
+        #endregion
+    }
+}
